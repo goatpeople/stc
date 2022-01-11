4493bbbeb56a164c52b2457b07342c89ebf84192
goatpeople <94653560+goatpeople@users.noreply.github.com>
2022-01-10 21:30:52 -0800

    Goatpeople/inventory (#6)
    * Added basic item and inventory system
    
    This definitely needs to be fleshed out, and main should include imports, but I'm just saving the past week of progress. Been working on the local branch for too long without committing.
    
    * QUESTIONS - Asking questions about item dicts and interactions
    
    I modified the 'item.py' file quite heavily in order to make answering questions easier.
    
    This is a tutorial commit, so no need to get fancy.

--- item.py
+++ item.py
@@ -1,73 +1,110 @@
+# I'll be making comments to explain my thought process
+# For clarity on questions, I'll mark them like this:
+
+### QUESTION HERE
+
 class Equip():
-  def __init__(self, name, desc, level, max_health, armor, priority, min_attack, max_attack, slot):
+  def __init__(self, name, slot):
     self.name = name   
-    self.description = desc   
-    self.level_requirement = level
-    self.max_health = max_health
-    self.armor = armor
-    self.priority = priority
-    self.min_attack = min_attack
-    self.max_attack = max_attack
     self.slot = slot
-
-
 
 class Inventory(object):    
   def __init__(self):
     self.bag = {}
+    self.equipped = {
+        "weapon" : "",
+        "shield" : "",
+    }
+inventory = Inventory()
 
-  def add_item(self, item):
-    if item in self.bag:
-      self.bag[item] += 1
-    else:
-      self.bag[item] = 1
-
-  def print_bag(self):
-    print('\t'.join(['Name', 'Quantity']))
-    print('\t'.join(['----', '--------']))
-    for item in inventory.bag:
-      print('\t'.join([str(x) for x in [item.name, inventory.bag[item]]]))
-
-
-
-
+# These are the items that can be equipped and stored in the bag, all with the "weapon" slot. I use that to access the "weapon" key in the 'inventory.equipped' dict.
 spear = Equip(
   name = "Spear",
-  desc = "A pointy stick",
-  level = 1,
-  max_health = 0,
-  armor = 0,
-  priority = 0,
-  min_attack = 3,
-  max_attack = 4,
   slot = "weapon"
 )
 rope = Equip(
   name = "Rope",
-  desc = "A pointy stick",
-  level = 1,
-  max_health = 0,
-  armor = 0,
-  priority = 0,
-  min_attack = 3,
-  max_attack = 4,
+  slot = "weapon"
+)
+hammer = Equip(
+  name = "Hammer",
+  slot = "weapon"
+)
+sword = Equip(
+  name = "Sword",
   slot = "weapon"
 )
 
-item_list = [spear, rope]
 
-inventory = Inventory()
+def add_item(item):
+  if item in inventory.bag:
+    inventory.bag[item] += 1
+  else:
+    inventory.bag[item] = 1
 
-inventory.add_item(spear)
-inventory.add_item(spear)
-inventory.add_item(rope)
-inventory.add_item(spear)
-inventory.add_item(spear)
-inventory.add_item(spear)
-inventory.add_item(spear)
-inventory.add_item(spear)
-print(inventory.bag)
-inventory.print_bag()
 
-playerinput = input("input: ")
+def equip_item(item):
+  if item in inventory.bag and inventory.bag[item] > 0:
+    # My goal is to remove the currently equipped item and put it back in the bag.
 
+    # I get a KeyError on the 'rope' object for modifying the dictionary twice (If I understand that corrently)
+    # This is the issue I am stuck on. I've tried creating a temporary dictionary, modifying that, and returning it, but I doubt I'm doing it correctly.
+    # I just don't know how to get around the KeyError without making a mess.
+
+    ### What would a proper function look like to accomplish this goal?
+    if inventory.equipped[item.slot] != "":
+      removed_item = inventory.equipped[item.slot]
+      inventory.bag[removed_item] += 1
+
+    inventory.bag[item] -= 1
+    inventory.equipped[item.slot] = item
+    
+  else:
+    print("Item is not in your inventory.")
+
+
+def print_bag():
+  print('\t'.join(['Name', 'Quantity']))
+  print('\t'.join(['----', '--------']))
+
+  # My goal here is one of two things:
+  # 1) Mainly, to print whatever items are in the bag and the quantity.
+  # 2) I thought it would be sneaky to clear all "0" quantity items here, because it would only ever display the bag when it is printing.
+  #    That way, the bag doesn't remember what items AREN'T in the bag and it looks cleaner, without removing 0 items from the dictionary
+
+  # I'm not sure if creating a temporary dictionary is a good idea here or not. Like the 'equip_item', I'm trying to get around a KeyError.
+  # At this point, I don't know the proper way to be able to modify separate dictionaries and have them operate in unison.
+  
+  ### Would it be a good idea to clear "0" values in the 'inventory.bag' dict after modifying it in any way (defining like a check_empty function and plugging that in), or clear them only when printing?
+  ### Like the 'equip_item' function above (line 46), what would be the proper way to modify a dictionary multiple times without getting a KeyError?
+
+  # I can feel that I am close, but after trying to figure this one issue out for the past week, I've run out of ideas.
+  temp_dict = inventory.bag
+  for item in inventory.bag:
+    if inventory.bag[item] == 0:
+      temp_dict = inventory.bag
+      temp_dict.pop(item)
+      return temp_dict
+    print('\t'.join([str(item) for item in [item.name, temp_dict[item]]]))
+  
+
+add_item(sword)
+add_item(hammer)
+add_item(rope)
+add_item(spear)
+
+# All four items are in the bag and properly displayed.
+print_bag()
+
+
+# First item is equipped, gets removed from the bag and placed into the 'inventory.equipped' dict with no problem, as expected.
+equip_item(spear)
+print_bag()
+print("")
+
+# This is where we run into the KeyError issue for modifying the dict too many times.
+equip_item(rope)
+print_bag()
+
+### I created this new file in order to try and import the functions to the 'main' loop, but how does the Inventory class (and item objects) get imported as well?
+#   I was having an issue with accessing the items when on a different file. The functions import perfectly, but I don't know if objects are able to be imported. (I hope I am using the terminology correctly!)
